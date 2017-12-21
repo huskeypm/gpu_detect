aa79bb45ade3eb04a96da9957daede7bf660e127
huskeypm <huskeypm@gmail.com>
2017-12-21 18:10:59 -0500

    changed myo paths\n added function for generating requisite filtesr

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
-## Todo
-see TODO for todo items 
 
 # gpu_detect
-
-Requires tensorflow cuda (python), which is currently installed on kafka and hesse.
-
 GPU-accelerated matched filter detection.
 
+Requires tensorflow cuda (python), which is currently installed on kafka and hesse.
 source config.bash to load tensor-flow libs
 
+
+## GPU 
 tensorflow_fftconv.py - compares performances of tensorflow FFT/matrix multiply/iFFT vs FFTPack
 
 tensorflow_mf.py - matched filtering via tensorflow
 
 commfigs.bash - generates images for paper 
 
-# validation test
+## Todo
+see TODO for todo items 
+
+## Myocyte 
+### validation test
 <code>
 python util.py -genWT -genLong -genLoss -genPunishment
-python validation.py -validation
+python myocyteFigs.py -validation
 </code>
 
 # generate paper figs
 <code>
-python validation.py -allFigs 
+python myocyteFigs.py -allFigs 
 </code>
 
 # normal test
@@ -33,10 +35,10 @@ python util.py -genWT -genLong -genLoss -genPunishment
 </code>
 ## - execute images 
 <code>
-python validation.py -tag "MI" -test ./images/WTFilter.png ./images/LongFilter.png ./images/MI_D_73_annotation.png 0.06 0.38 3.
+python myocyteFigs.py -tag "MI" -test ./images/WTFilter.png ./images/LongFilter.png ./images/MI_D_73_annotation.png 0.06 0.38 3.
 </code>
 
-### PNP project
+## PNP project
 <code>
 python silicaFigs.py -rocfigs 
 </code>
@@ -49,5 +51,5 @@ python figs.py -paperfigs # generate figures for paper
 -- May be broken, but see sparkdetection repo for working version 
 </code>
 
-### Other notes
+## Other notes
 optimizer.py was copied from spark detection repo, which was design for silica films 
--- myocyteFigs.py
+++ myocyteFigs.py
@@ -10,8 +10,140 @@ import sys
 #
 # ROUTINE  
 #
-def doit(fileIn):
-  1
+import ROCstacker as Rs
+import numpy as np
+import cv2
+import matplotlib.pylab as plt
+class empty:pass
+
+## WT 
+def fig3(): 
+
+  print "DC: need to commit WT data to repo and pass in here" 
+  figAnalysis(
+    testImage="./images/MI_D_73_annotation.png",
+    tag = "MI", 
+    writeImage=True) 
+
+  # write angle 
+  print "DC: print mesh of angles here" 
+
+
+## HF 
+def fig4(): 
+
+  print "DC: need to commit HF data to repo and pass in here" 
+  figAnalysis(
+    testImage="./images/MI_D_73_annotation.png",
+    tag = "MI", 
+    writeImage=True) 
+
+## MI 
+def fig5(): 
+
+  print "DC: need to commit prox, med, dist MI data to repo and pass in here" 
+  print "DC: combine bar plots into single image?"
+  figAnalysis(
+    testImage="./images/MI_D_73_annotation.png",
+    tag = "MIprox", 
+    writeImage=True) 
+
+  figAnalysis(
+    testImage="./images/MI_D_73_annotation.png",
+    tag = "MImed", 
+    writeImage=True) 
+
+  figAnalysis(
+    testImage="./images/MI_D_73_annotation.png",
+    tag = "MIdist", 
+    writeImage=True) 
+
+
+
+
+def figAnalysis(
+      ttFilterName="./images/WTFilter.png",
+      ltFilterName="./images/LongFilter.png",
+      testImage="./images/MI_D_73_annotation.png",
+      ttThresh=0.06 ,
+      ltThresh=0.38 ,
+      gamma=3.,
+      tag = "valid", # tag to prepend to images 
+      writeImage = False):
+
+  results = testMF(
+      ttFilterName=ttFilterName,#"./images/WTFilter.png",
+      ltFilterName=ltFilterName,#"./images/LongFilter.png",
+      testImage=testImage,#"./images/MI_D_73_annotation.png",
+      ttThresh=ttThresh,#0.06 ,
+      ltThresh=ltThresh,#0.38 ,
+      gamma=gamma)        
+
+  stackedHits = results.stackedHits
+
+  # report responses for each channel   
+  dimensions = np.shape(stackedHits.WT)
+  area = float(dimensions[0] * dimensions[1])
+  results.ttContent = np.sum(stackedHits.WT)/ area
+  #print results.ttContent
+  results.ltContent = np.sum(stackedHits.Long) / area
+  #print results.ltContent
+  results.lossContent = 0.
+
+
+  # write bar plot of feature contnent  
+  fig, ax = plt.subplots()
+  ax.set_title("% content") 
+  values= np.array([ results.ttContent, results.ltContent, results.lossContent])
+  values = values/np.max( values ) 
+  ind = np.arange(np.shape ( values )[0])
+  width = 1.0
+  color = ["blue","green","red"] 
+  marks = ["WT","LT","Loss"] 
+  rects = ax.bar(ind, values, width,color=color)   
+  ax.set_xticks(ind+width)
+  ax.set_xticklabels( marks ,rotation=90 )
+  plt.gcf().savefig(tag+"_content.png") 
+
+
+
+    
+
+
+def testMF(
+      ttFilterName="./images/WTFilter.png",
+      ltFilterName="./images/LongFilter.png",
+      testImage="./images/MI_D_73_annotation.png",
+      ttThresh=0.06 ,
+      ltThresh=0.38 ,
+      gamma=3.,
+      tag = "default_",
+      writeImage = False):
+
+  #results = empty()
+  results = Rs.giveStackedHits(testImage, 
+		   ttThresh, ltThresh, gamma, WTFilterName=ttFilterName,
+                   LongitudinalFilterName=ltFilterName)
+  stackedHits = results.stackedHits
+
+  # BE SURE TO REMOVE ME!!
+  print "WARNING: nan returned from stackedHits, so 'circumventing this'"
+  cI = results.coloredImg
+  wt = np.zeros_like( cI[:,:,0]) 
+  wt[ np.where(cI[:,:,2] > 100) ] = 1
+  results.ttContent = stackedHits.WT = wt 
+  lt = np.zeros_like( cI[:,:,0]) 
+  lt[ np.where(cI[:,:,1] > 100) ] = 1   
+  stackedHits.Long = lt 
+
+  if writeImage:
+    # write putputs	  
+    cv2.imwrite(tag+"_output.png",results.coloredImg)       
+
+
+  return results 
+
+  
 
 
 #
@@ -36,6 +168,7 @@ Notes:
 #
 # MAIN routine executed when launching this script from command line 
 #
+tag = "default_" 
 if __name__ == "__main__":
   import sys
   msg = helpmsg()
@@ -44,13 +177,13 @@ if __name__ == "__main__":
   if len(sys.argv) < 2:
       raise RuntimeError(msg)
 
-  #fileIn= sys.argv[1]
-  #if(len(sys.argv)==3):
-  #  1
-  #  #print "arg"
-
   # Loops over each argument in the command line 
   for i,arg in enumerate(sys.argv):
+    # will return a single marked image 
+    if(arg=="-validation"):
+      testmf()     
+      print "WARNING: add unit test here!!" 
+      quit()
 
     # this function will generate input data for the current fig #3 in the paper 
     if(arg=="-fig3"):               
@@ -69,10 +202,28 @@ if __name__ == "__main__":
       # RB: generate detected version of Fig 6
       # PKH: add in scaling plot 
 
-      # apply TT, LT and loss filters to 
-      #doit(arg1)
-  
+    # generates all figs
+    if(arg=="-allFigs"):
+      fig3()     
+      fig4()     
+      fig5()
+      fig6()     
+      quit()
 
+    if(arg=="-tag"):
+      tag = sys.argv[i+1]
+	   
+    if(arg=="-test"):
+      testMF(      
+        ttFilterName=sys.argv[i+1],
+        ltFilterName=sys.argv[i+2],
+        testImage=sys.argv[i+3],           
+        ttThresh=np.float(sys.argv[i+4]),           
+        ltThresh=np.float(sys.argv[i+5]),
+        gamma=np.float(sys.argv[i+6]),
+	tag = tag,
+	writeImage = True)            
+      quit()
 
 
 

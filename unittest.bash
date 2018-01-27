9b03c4f85ec115d1ad343bea2214d53efab94c80
Dfco222 <dfco222@g.uky.edu>
2018-01-27 18:54:00 -0500

    Fixed both of myocyteFigs validation routines and added in a function to calculate the content of each filter according to their marked channel

--- unittest.bash
+++ unittest.bash
@@ -1,10 +1,8 @@
 # Run these before committing 
 python silicaFigs.py -validate
 python myocyteFigs.py -roc                   
-echo "broke"; python myocyteFigs.py -minorValidate
-echo "broke"; python myocyteFigs.py -validate
+python myocyteFigs.py -minorValidate
+python myocyteFigs.py -validate
 python myocyteFigs.py -scoretest
 python tissue.py -validate
 python optimizer.py -optimizeLight  # move these to silica figs
-
-

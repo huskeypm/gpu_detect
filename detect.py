597669f5c3b349a46f7b8b647e92f873bbe8d331
Dfco222 <dfco222@g.uky.edu>
2018-06-25 10:20:49 -0400

    Made strides in improving the yaml functionality for webserver

--- detect.py
+++ detect.py
@@ -14,6 +14,7 @@ import painter
 import sys
 import yaml
 import cv2
+import preprocessing as pp
 
 def DisplayHits(img,threshed,
                 smooth=8 # px
@@ -51,9 +52,6 @@ def docalc(img,
 
 
     results = bD.DetectFilter(inputs,paramDict,iters=iters,display=debug)
-    result = results.correlated[0]
-    #corr = np.asarray(results.correlated[0],dtype=float) # or
-    results.threshed = results.stackedHits
 
     pasteFilter = True
     if pasteFilter:
@@ -61,10 +59,7 @@ def docalc(img,
       MFy,MFx = util.measureFilterDimensions(mf)
       filterChannel = 0
       imgDim = np.shape(img)
-      #coloredImageHolder = np.zeros((imgDim[0],imgDim[1],3),dtype=npfloat64)
-      #filterChannelHolder = coloredImageHolder[:,:,filterChannel)
-      # TODO: Come back and fix thresh
-      results.threshed = painter.doLabel(results,dx=MFx,dy=MFy,thresh=254)
+      results.threshed = painter.doLabel(results,dx=MFx,dy=MFy,thresh=paramDict['snrThresh'])
       #coloredImageHolder[:,:,filterChannel] = filterChannelHolder
     
     print "Writing file %s"%fileName
@@ -88,7 +83,7 @@ def docalc(img,
 #
 # Simple performs a matched filtering detection with a single filter, image and threshold
 #
-def simple(imgName,mfName,thresh,debug=False,smooth=4,outName="hits.png"): 
+def simple(imgName,mfName,filterTwoSarcomereSize,thresh,debug=False,smooth=4,outName="hits.png"): 
   img = util.ReadImg(imgName,renorm=True)
   mf  = util.ReadImg( mfName,renorm=True)
 
@@ -123,9 +118,87 @@ def simpleYaml(ymlName):
   simple(
     data['imgName'],
     data['mfName'],
+    data['filterTwoSarcomereSize'],
     data['thresh'],
     debug= data['debug'],
     outName=outName)
+
+###
+### updated yaml call
+###
+def updatedSimpleYaml(ymlName):
+  print "Adapt to accept filter mode argument?"
+  with open(ymlName) as fp:
+    data = yaml.load(fp)
+  print "Reading %s" % ymlName
+
+  if 'outName' in data:
+    outName = data['outName']
+  else:
+    outName = 'hits.png'
+  
+  updatedSimple(data['imgName'],
+                data['mfName'],
+                data['filterTwoSarcomereSize'],
+                data['thresh'],
+                debug=data['debug'],
+                outName=outName
+                )
+
+###
+###  Updated YAML routine to lightly preprocess image
+###
+def updatedSimple(imgName,mfName,filterTwoSarcomereSize,threh,debug=False,smooth=4,outName="hits.png"):
+  '''
+  Updated routine for the athena web server that utilizes WT punishment filter.
+
+  INPUTS:
+    - imgName: Name of image that has myocyte of interest in the middle of the image
+               Myocyte can be of any orientation BUT there should be some conserved 
+               striations in the middle of the image. 
+    - thresh: Threshold that is utilized in the detection
+  
+  OUTPUTS:
+    - None, writes image
+  '''
+  ### Load necessary inputs
+  img = util.ReadImg(imgName)
+  imgDims = np.shape(img)
+  #mf = util.LoadFilter("./myoimages/newSimpleWTFilter")
+  mf = util.LoadFilter(mfName)
+  mfPunishment = util.LoadFilter("./myoimages/newSimpleWTPunishmentFilter.png")
+
+  ### Lightly preprocess the image
+  # reorient image using PCA
+  #img = pp.autoReorient(img)
+  # grab subsection for resizing image. Extents are just guesses so they could be improved
+  cY, cX = int(round(float(imgDims[0]/2.))), int(round(float(imgDims[1]/2.)))
+  xExtent = 50
+  yExtent = 25
+  top = cY-yExtent; bottom = cY+yExtent; left = cX-xExtent; right = cX+xExtent
+  indexes = np.asarray([top,bottom,left,right])
+  subsection = np.asarray(img[top:bottom,left:right],dtype=np.float64)
+  subsection /= np.max(subsection)
+  img, scale, newIndexes = pp.resizeGivenSubsection(img,subsection,filterTwoSarcomereSize,indexes)
+  # intelligently threshold image using gaussian thresholding
+  img = pp.normalizeToStriations(img, newIndexes, filterTwoSarcomereSize)
+  img = np.asarray(img,dtype=np.float64)
+  img /= np.max(img)
+
+  ### Construct parameter dictionary
+  paramDict = optimizer.ParamDict(typeDict="WT")
+  paramDict['mfPunishment'] = mfPunishment
+  paramDict['covarianceMatrix'] = np.ones_like(img)
+
+  docalc(img,
+         mf,
+         paramDict=paramDict,
+         debug=debug,
+         iters=[-25,-20,-15,-10,-5,0,5,10,15,20,25],
+         smooth=smooth,
+         fileName = outName
+         )
+
 
 
 ###
@@ -330,6 +403,12 @@ if __name__ == "__main__":
     if(arg=="-simpleYaml"):             
       ymlName=sys.argv[i+1] 
       simpleYaml(ymlName)
+      quit()
+
+    # updated version of simple yaml
+    if(arg=="-updatedSimpleYaml"):
+      ymlName = sys.argv[i+1]
+      updatedSimpleYaml(ymlName)
       quit()
 
     if(arg=="-giveCorrelation"):

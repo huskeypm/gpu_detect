Fixed issue with rotation of the filter in the 2D GPU implementation

myocyteFigs.py
@@ -469,7 +469,7 @@ def figS3():
   displayTissueCaseHits(caseGPU,"figS3_GPU")
 
   ### store results
-  GPUstackedHits = caseGPU.results.stackedHits
+  GPUpastedHits = caseGPU.pasted
 
   ### Preprocess and analyze the case
   caseCPU = analyzeTissueCase(caseCPU,preprocess=False,useGPU=False)
@@ -478,7 +478,7 @@ def figS3():
   displayTissueCaseHits(caseCPU,"figS3_CPU")
 
   ### store results
-  CPUstackedHits = caseCPU.results.stackedHits
+  CPUpastedHits = caseCPU.pasted
 
   ### save original enhanced image for comparison
   plt.figure()
@@ -486,7 +486,7 @@ def figS3():
   plt.gcf().savefig('figS3_enhancedImg.png',dpi=300)
   
   ### do comparison between GPU and CPU results
-  comparison = np.abs(GPUstackedHits - CPUstackedHits)
+  comparison = np.abs(GPUpastedHits - CPUpastedHits)
   plt.figure()
   plt.imshow(comparison,cmap='gray')
   plt.colorbar()

threeDtense.py
@@ -253,7 +253,6 @@ def doTFloop(inputs,
     bigIters = np.asarray(bigIters,dtype=np.float32)
     bigIters = bigIters * np.pi / 180.
 
-
     # have to convert to a tensor so that the rotations can be indexed during tf while loop
     bigIters = tf.Variable(tf.convert_to_tensor(bigIters,dtype=tf.float32))
 
@@ -298,14 +297,25 @@ def doTFloop(inputs,
 
     def body2D(cnt,stackedHits,bestAngles):
       rotation = bigIters[cnt]
-
-      inputs.rotatedMF = util.rotateTFFilter2D(inputs.tfFilt,rotation)
+ 
+      ### center filter for rotation scheme
+      shiftY = inputs.imgOrig.shape[0]/2
+      shiftX = inputs.imgOrig.shape[1]/2
+      inputs.rotatedMF = tf.manip.roll(inputs.tfFilt,shift=[shiftY,shiftX],axis=[0,1])
+      ### rotate filter
+      inputs.rotatedMF = util.rotateTFFilter2D(inputs.rotatedMF,rotation)
+      ### shift filter again
+      inputs.rotatedMF = tf.manip.roll(inputs.rotatedMF,shift=[shiftY,shiftX],axis=[0,1])
       ### shift the rotated mf
       #inputs.rotatedMF = tf.manip.roll(inputs.rotatedMF,
       #        shift=[inputs.imgOrig.shape[0],inputs.imgOrig.shape[1]],axis=[0,1])
 
       if paramDict['filterMode'] == "punishmentFilter":
-        inputs.rotatedPunishment = util.rotateTFFilter2D(paramDict['mfPunishment'],rotation)
+        ### center filter for rotation scheme
+        inputs.rotatedPunishment = tf.manip.roll(paramDict['mfPunishment'],shift=[shiftY,shiftX],axis=[0,1])
+        inputs.rotatedPunishment = util.rotateTFFilter2D(inputs.rotatedPunishment,rotation)
+        inputs.rotatedPunishment = tf.manip.roll(inputs.rotatedPunishment,shift=[shiftY,shiftX],axis=[0,1])
+
         ### shift the rotated punishment mf
         #inputs.rotatedPunishment = tf.manip.roll(inputs.rotatedPunishment,
         #        shift=[inputs.imgOrig.shape[0],inputs.imgOrig.shape[1]],axis=[0,1])